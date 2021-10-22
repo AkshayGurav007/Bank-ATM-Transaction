e4b591c036c36133a7bf05ec6ec61d1fba3e49f6
Akshay Gurav <89732894+AkshayGurav007@users.noreply.github.com>
2021-10-22 17:37:44 +0530

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,24 @@
+#  Before withdraw starts, validate PIN number
+#  For withdraw to be success,the balance should be sufficient
+
+pin=1234
+accbal = 10000.00
+
+input_pin = int(input("Enter the PIN\n"))
+
+if pin==input_pin:
+    print("Valid PIN")
+    amt = float(input("enter the amount to be withdrawn\n"))
+    print("Withdrawing Rs ",amt)
+
+    if amt < accbal:
+        accbal = accbal - amt
+        print("Withdraw success")
+    else:
+        print("Withdraw Failed")
+        print("Insufficient Balance in account")
+
+    print("Account Balance is Rs ",accbal)
+else:
+    print("Invalid PIN")
+
